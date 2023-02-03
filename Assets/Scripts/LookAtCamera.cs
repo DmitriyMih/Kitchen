a6624b87c1e571abbf1bc129836b96be45bcffab
Jew <dima.com655@gmail.com>
2023-02-04 00:30:55 +0300

    Add Bar Look At

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class LookAtCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private enum LookMode
     {
-        
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
+    }
+    [SerializeField] private LookMode lookMode;
+
+    private void LateUpdate()
+    {
+        if (Camera.main == null)
+            return;
+
+        switch(lookMode)
+        {
+            case LookMode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+
+            case LookMode.LookAtInverted:
+                Vector3 dirFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + dirFromCamera);
+                break;
+
+            case LookMode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+
+            case LookMode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
