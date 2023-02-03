e27ac718dfd10ce78616c48e2c158a5ea02a9f93
Jew <dima.com655@gmail.com>
2023-02-04 01:33:55 +0300

    Add Trash Counter

---
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TrashCounter : BaseCounter
+{
+    [SerializeField] private Transform bottomPoint;
+    [SerializeField] private float objectMoveTime = 0.4f;
+    [SerializeField] private float objectScaleTime = 0.4f;
+
+    public override void Interact(PlayerController player)
+    {
+        if (player.HasKitchenObject())
+        {
+            player.GetKitchenObject().SetKitchenObjectParent(this, false);
+            KitchenObject kitchenObject = GetKitchenObject();
+
+            kitchenObject.transform.DOScale(Vector3.zero, objectScaleTime);
+            kitchenObject.transform.DOMove(bottomPoint.position, objectMoveTime).OnComplete(()=> kitchenObject.DestroySelf());
+        }
+    }
+
+}
