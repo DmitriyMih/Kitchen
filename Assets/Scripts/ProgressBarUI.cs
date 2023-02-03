58f149fff361ffaf672aa3819527513aa0d1939c
Jew <dima.com655@gmail.com>
2023-02-03 22:47:13 +0300

    Add DgTween Pack

--- Assets/Scripts/ProgressBarUI.cs
+++ Assets/Scripts/ProgressBarUI.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using DG.Tweening;
 public class ProgressBarUI : MonoBehaviour
 {
     [SerializeField] private CuttingCounter cuttingCounter;
     [SerializeField] private Image fillImage;
 
     [SerializeField] private GameObject barGroup;
+    [SerializeField] private float fillingTime = 0.1f;
 
     private void Start()
     {
@@ -29,7 +30,7 @@ public class ProgressBarUI : MonoBehaviour
         if (fillImage == null) { Debug.LogError("Fill Image Is Null"); return; }
 
         value = Mathf.Clamp(value, 0f, 1f);
-        fillImage.fillAmount = value;
+        fillImage.DOFillAmount(value, fillingTime);
 
         if (value == 0f || value == 1f)
             Hide();
@@ -40,12 +41,16 @@ public class ProgressBarUI : MonoBehaviour
     private void Show()
     {
         if (barGroup == null) { Debug.Log("Bar Group Is Null"); return; }
+        
         barGroup.SetActive(true);
     }
 
     private void Hide()
     {
         if (barGroup == null) { Debug.Log("Bar Group Is Null"); return; }
+        if (fillImage == null) { Debug.LogError("Fill Image Is Null"); return; }
+
         barGroup.SetActive(false);
+        fillImage.fillAmount = 0;
     }
 }
