e1d25731203ae37cf1675b8a16ad7841ea63f988
Jew <dima.com655@gmail.com>
2023-02-05 10:00:47 +0300

    Add Plate Logic In Clean Counter

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -16,23 +16,100 @@ public class ClearCounter : BaseCounter
         {
             if (player.HasKitchenObject())
             {
-                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+                //  If Plate In Hand
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObjectInHand))
                 {
-                    //  If Plate In Hand
-                    if (plateKitchenObject.TryAddIngridient(GetKitchenObject().GetKitchenObjectSO()))
-                        GetKitchenObject().DestroySelf();
+                    //  Plate On Counter
+                    if (GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObjectInCounter))
+                    {
+                        //Debug.Log($"In Hand {plateKitchenObjectInHand.GetKitchenObjectSOListCount()}");
+                        //Debug.Log($"In Counter {plateKitchenObjectInCounter.GetKitchenObjectSOListCount()}");
+
+                        //  In Hand Empty
+                        if (!plateKitchenObjectInHand.PlateIsNotEmpty())
+                        {
+                            Debug.Log(" Hand Empty");
+                            ChangeTheContentsOfThePlates(plateKitchenObjectInHand, plateKitchenObjectInCounter);
+                        }
+                        //  Plate On Counter Not Null
+                        else if (!plateKitchenObjectInCounter.PlateIsNotEmpty())
+                        {
+                            Debug.Log("Counter Empty");
+                            ChangeTheContentsOfThePlates(plateKitchenObjectInHand, plateKitchenObjectInCounter);
+                        }
+                        else
+                        {
+                            if (TheKitchenObjectContentVaries(plateKitchenObjectInCounter, plateKitchenObjectInHand))
+                                AddContentToKitchenPlates(plateKitchenObjectInCounter, plateKitchenObjectInHand);
+                        }
+                    }
+                    //  Plate not on the table
+                    else
+                    {
+                        if (plateKitchenObjectInHand.TryAddIngridient(GetKitchenObject().GetKitchenObjectSO()))
+                            GetKitchenObject().DestroySelf();
+                    }
                 }
+                //  In Hand No Plate
                 else
                 {
-                    if (GetKitchenObject().TryGetPlate(out plateKitchenObject))
-                        if (plateKitchenObject.TryAddIngridient(player.GetKitchenObject().GetKitchenObjectSO()))
+                    if (GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObjectInCounter))
+                    {
+                        if (plateKitchenObjectInCounter.TryAddIngridient(player.GetKitchenObject().GetKitchenObjectSO()))
                             player.GetKitchenObject().DestroySelf();
                         else
                             Debug.Log("Not Find | " + player.GetKitchenObject().GetKitchenObjectSO());
+                    }
                 }
             }
             else
                 GetKitchenObject().SetKitchenObjectParent(player);
         }
+    }
+
+    private void AddContentToKitchenPlates(PlateKitchenObject firstKitchenObjet, PlateKitchenObject secondKitchenObjet)
+    {
+        List<KitchenObjectSO> tempSecondKitchenObjectSO = new List<KitchenObjectSO>(secondKitchenObjet.GetKitchenObjectSOList());
+        secondKitchenObjet.ClearThePlate();
+
+        foreach (KitchenObjectSO kitchenObjectSO in tempSecondKitchenObjectSO)
+            firstKitchenObjet.TryAddIngridient(kitchenObjectSO);
+    }
+
+    private void ChangeTheContentsOfThePlates(PlateKitchenObject firstKitchenObjet, PlateKitchenObject secondKitchenObjet)
+    {
+        List<KitchenObjectSO> tempFirstKitchenObjectSO = new List<KitchenObjectSO>(firstKitchenObjet.GetKitchenObjectSOList());
+        List<KitchenObjectSO> tempSecondKitchenObjectSO = new List<KitchenObjectSO>(secondKitchenObjet.GetKitchenObjectSOList());
+
+        firstKitchenObjet.ClearThePlate();
+        secondKitchenObjet.ClearThePlate();
+
+        foreach (KitchenObjectSO kitchenObjectSO in tempFirstKitchenObjectSO)
+            secondKitchenObjet.TryAddIngridient(kitchenObjectSO);
+
+        foreach (KitchenObjectSO kitchenObjectSO in tempSecondKitchenObjectSO)
+            firstKitchenObjet.TryAddIngridient(kitchenObjectSO);
+    }
+
+    private bool TheKitchenObjectContentVaries(PlateKitchenObject firstKitchenObjet, PlateKitchenObject secondKitchenObject)
+    {
+        List<KitchenObjectSO> tempKitchenObjectSO = firstKitchenObjet.GetKitchenObjectSOList();
+
+        for (int i = 0; i < tempKitchenObjectSO.Count; i++)
+        {
+            if (tempKitchenObjectSO[i] == null)
+            {
+                Debug.Log($"Kitchien Object {i} Is Null");
+                return false;
+            }
+
+            if (secondKitchenObject.FindElemetInKitchenList(tempKitchenObjectSO[i]))
+            {
+                Debug.Log($"Exit In {i} | {tempKitchenObjectSO[i]} Element");
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -37,6 +37,27 @@ public class PlateKitchenObject : KitchenObject
         return true;
     }
 
+    public bool FindElemetInKitchenList(KitchenObjectSO kitchenObjectSO)
+    {
+        if (kitchenObjectSO == null) { Debug.LogError("Kitchen Object Is Null | " + gameObject.name); return false; }
+
+        //Debug.Log($"Element {kitchenObjectSO} | Start");
+        if (!validKitchenObjectSOList.Contains(kitchenObjectSO))
+            return false;
+
+        //Debug.Log($"Element {kitchenObjectSO} | Middle");
+        if (kitchenObjectSOList.Contains(kitchenObjectSO))
+            return true;
+
+        //Debug.Log($"Element {kitchenObjectSO} | End");
+        return false;
+    }
+
+    public int GetKitchenObjectSOListCount()
+    {
+        return kitchenObjectSOList.Count;
+    }
+
     public bool PlateIsNotEmpty()
     {
         return kitchenObjectSOList.Count != 0;
